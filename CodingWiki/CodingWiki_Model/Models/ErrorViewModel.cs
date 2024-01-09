a1ce71db68de34082e041a480bf916dadabe2fc8
gabriel <gabriel.pirastru@devoteam.com>
2024-01-09 16:44:12 +0100

    added folder structure

---
+++ CodingWiki/CodingWiki_Model/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodingWiki_Model.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
